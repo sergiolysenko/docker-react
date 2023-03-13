01dc2ef8ba564cdd3e1b703cd582a8ea1dea2f44
sergiolysenko <sergeylysenko1406@gmail.com>
2023-03-13 19:48:09 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          E1212awdawdawd1212dit <code>src/App.js</code> and ololo save to reload
+          111.
         </p>
         <a
           className="App-link"
