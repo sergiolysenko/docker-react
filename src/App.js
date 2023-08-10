f96b0139cc7cce7393b4b5cb4128381908fba2f4
sergiolysenko <sergeylysenko1406@gmail.com>
2023-08-10 09:14:07 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          E1212awdawdawd1212dit <code>src/App.js</code> and ololo save to reload
-          111.
+          MY NAME IS<code>src/App.js</code> and ololo save to reload 111.
         </p>
         <a
           className="App-link"
